3d8b19ba091408e142c1fa707891073fb9c2b0a9
Colk-tech <master@colk.dev>
2025-08-19 02:38:42 +0900

    fix: add `seconds=0` for `ge` of `INTERVAL_FOR_SAME_TARGET_SEC`

--- src/domain/interface/config/domain.py
+++ src/domain/interface/config/domain.py
@@ -10,7 +10,7 @@ class DomainConfigIF(ConfigIF):
         ge=0.0, lt=1.0, description="Probability to react to a message"
     )
     INTERVAL_FOR_SAME_TARGET_SEC: timedelta = Field(
-        ge=timedelta(), description="Time interval to react to a same target"
+        ge=timedelta(seconds=0), description="Time interval to react to a same target"
     )
     DO_REACT_TO_GROUP: bool = Field(
         default=False, description="Whether or not to react to a group"
