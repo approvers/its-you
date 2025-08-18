24d2dadf8b4a0e37fe8d8c954390bd10db5070ff
Colk-tech <master@colk.dev>
2025-08-19 02:37:28 +0900

    fix: run `ruff format`

--- src/domain/interface/config/domain.py
+++ src/domain/interface/config/domain.py
@@ -7,18 +7,14 @@ from src.config.interface import ConfigIF
 
 class DomainConfigIF(ConfigIF):
     REACT_PROBABILITY: float = Field(
-        ge=0.0, lt=1.0,
-        description="Probability to react to a message"
+        ge=0.0, lt=1.0, description="Probability to react to a message"
     )
     INTERVAL_FOR_SAME_TARGET_SEC: timedelta = Field(
-        ge=timedelta(),
-        description="Time interval to react to a same target"
+        ge=timedelta(), description="Time interval to react to a same target"
     )
     DO_REACT_TO_GROUP: bool = Field(
-        default=False,
-        description="Whether or not to react to a group"
+        default=False, description="Whether or not to react to a group"
     )
     DO_REACT_TO_USER: bool = Field(
-        default=True,
-        description="Whether or not to react to a user"
+        default=True, description="Whether or not to react to a user"
     )
