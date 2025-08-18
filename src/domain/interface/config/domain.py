d229d1e2c4f22e0e56b9a92ee53df18ca056f3e6
Colk-tech <master@colk.dev>
2025-08-19 02:36:32 +0900

    fix: fix redundant default values

--- src/domain/interface/config/domain.py
+++ src/domain/interface/config/domain.py
@@ -7,11 +7,11 @@ from src.config.interface import ConfigIF
 
 class DomainConfigIF(ConfigIF):
     REACT_PROBABILITY: float = Field(
-        ge=float(0), lt=float(1),
+        ge=0.0, lt=1.0,
         description="Probability to react to a message"
     )
     INTERVAL_FOR_SAME_TARGET_SEC: timedelta = Field(
-        ge=timedelta(minutes=0),
+        ge=timedelta(),
         description="Time interval to react to a same target"
     )
     DO_REACT_TO_GROUP: bool = Field(
