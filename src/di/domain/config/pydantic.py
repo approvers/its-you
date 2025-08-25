154e9c04924db55923a09de851e101eaa4d729d3
Colk-tech <master@colk.dev>
2025-08-26 01:15:15 +0900

    add: add `PydanticDomainConfigModule`

---
+++ src/di/domain/config/pydantic.py
@@ -0,0 +1,15 @@
+from injector import SingletonScope
+
+from config import ItsYouConfig
+from src.di.builder import ModuleBase, BindEntry
+from src.domain.config.interface import DomainConfigIF
+
+
+class PydanticDomainConfigModule(ModuleBase):
+    _BINDINGS = (
+        BindEntry(
+            interface=DomainConfigIF,
+            to=ItsYouConfig,
+            scope=SingletonScope,
+        )
+    )
