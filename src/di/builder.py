0b2d7ef00ba17ce2f005a0dcbd4b3dc5e3f5f33b
Colk-tech <master@colk.dev>
2025-08-26 00:50:44 +0900

    add: implement `ModuleBase`

---
+++ src/di/builder.py
@@ -0,0 +1,32 @@
+import dataclasses
+from typing import TYPE_CHECKING, Generic, TypeVar
+
+from injector import Module
+
+if TYPE_CHECKING:
+    from typing import Union, Iterable
+    from abc import ABC
+
+    from injector import Scope, ScopeDecorator, Binder
+
+
+InterfaceT = TypeVar("InterfaceT", bound="ABC")
+
+
+@dataclasses.dataclass
+class BindEntry(Generic[InterfaceT]):
+    interface: type[InterfaceT]
+    to: type[InterfaceT]
+    scope: "Union[None, type['Scope'], 'ScopeDecorator']" = None
+
+
+class ModuleBase(Module):
+    _BINDINGS: "Iterable[BindEntry[ABC]]"
+
+    def configure(self, binder: "Binder") -> None:
+        for entry in self._BINDINGS:
+            binder.bind(
+                interface=entry.interface,
+                to=entry.to,
+                scope=entry.scope,
+            )
