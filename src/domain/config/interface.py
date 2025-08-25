63f374742de1763007656e87009afe39ad27cc29
Colk-tech <master@colk.dev>
2025-08-26 01:05:18 +0900

    remove: remove not required configs in `DomainConfigIF`

--- src/domain/config/interface.py
+++ src/domain/config/interface.py
@@ -1,27 +1,6 @@
-from abc import abstractmethod
-from datetime import timedelta
-
 from src.config.interface import ConfigIF
 
 
 # noinspection PyPep8Naming
 class DomainConfigIF(ConfigIF):
-    @property
-    @abstractmethod
-    def REACT_PROBABILITY(self) -> float:
-        pass
-
-    @property
-    @abstractmethod
-    def INTERVAL_FOR_SAME_TARGET_SEC(self) -> timedelta:
-        pass
-
-    @property
-    @abstractmethod
-    def DO_REACT_TO_GROUP(self) -> bool:
-        pass
-
-    @property
-    @abstractmethod
-    def DO_REACT_TO_USER(self) -> bool:
-        pass
+    pass
